5245a90989c7365d1d4422d995b2bcf531305e54
duyan <3375836164@qq.com>
2023-02-22 12:47:07 +0800

    modified:   Demo.py 	modified:   pyghost.pyc 	Demo 10x.exe 	Demo 15x.exe

--- Demo.py
+++ Demo.py
@@ -1,21 +1,20 @@
 import pyghost as wanna
-from pygame.image import load
-from pygame.transform import flip
+import pygame
 import os
 
 
 def loadimgs():  # 这个是测试时加载的图片，都是除了room，其他都是32*32
-    wanna.imgs['mask'] = load('ghost/kid/Mask.png')
-    wanna.imgs['save'] = load('ghost/other/Save.png')
-    wanna.imgs['room'] = load('ghost/rooms/Room.png')
+    wanna.imgs['mask'] = pygame.image.load('ghost/kid/Mask.png')
+    wanna.imgs['save'] = pygame.image.load('ghost/other/Save.png')
+    wanna.imgs['room'] = pygame.image.load('ghost/rooms/Room.png')
     animate: dict[str, str] = {}
     for i in ['idle', 'run', 'jump', 'fall']:
         for j in enumerate(wanna.addrs['kid'][i]):
             animate[f'{i}r{j[0]}'] = f'{i}r{(j[0]+1)%len(wanna.addrs["kid"][i])}'
-            wanna.imgs[f'{i}r{j[0]}'] = load(os.path.join(
+            wanna.imgs[f'{i}r{j[0]}'] = pygame.image.load(os.path.join(
                 'ghost', 'kid', j[1]))
             animate[f'{i}l{j[0]}'] = f'{i}l{(j[0]+1)%len(wanna.addrs["kid"][i])}'
-            wanna.imgs[f'{i}l{j[0]}'] = flip(
+            wanna.imgs[f'{i}l{j[0]}'] = pygame.transform.flip(
                 wanna.imgs[f'{i}r{j[0]}'], True, False)
     return animate
 
@@ -42,17 +41,18 @@ def init():  # 初始化最好传给wanna.start
 
     save1 = wanna.Trigger('save', (24*32, 9*32), touch={
                           'player'}, affairs=[spawn])
-    wanna.rooms.append(wanna.Room((0+400, 17*32-100),
+    wanna.rooms.append(wanna.Room((0, 17*32),
                        kid, [test, save1]))  # 默认重生位置
     wanna.room = 0  # 初始房间
 
 
 # 帧数15 建议debug=True时计时用的是thread time，虽然cpu占用会高，但不会波动
-@wanna.start(init, (25*32, 19*32), 'Ghost Engine Demo', cps=5, debug=True)
+@wanna.start(init, (25*32, 19*32), 'Ghost Engine Demo', cps=15, debug=True)
 def update(live: tuple[int, int], orders: list[wanna.Obj], pressed: tuple[int, ...], newindex: int) -> None:
-    print(live)  # 一秒内多少帧和上一帧经过的毫秒数
-    for i in orders:
-        if isinstance(i, wanna.Player):
-            print(i.pos)  # 打印kid坐标
-    if pygame.K_RETURN in pressed and pressed.index(pygame.K_RETURN) >= newindex:
-        print(pressed, newindex)  # >=newindex为这一帧刚按下
+    # print(live)  # 一秒内多少帧和上一帧经过的毫秒数
+    # for i in orders:
+    #     if isinstance(i, wanna.Player):
+    #         print(i.pos)  # 打印kid坐标
+    # if pygame.K_RETURN in pressed and pressed.index(pygame.K_RETURN) >= newindex:
+    #     print(pressed, newindex)  # >=newindex为这一帧刚按下
+    print(orders[-1]._Player__hv, orders[-1]._Player__b)
